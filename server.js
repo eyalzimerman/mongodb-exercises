1fedb9ed3b31aa15c4148ac0c9005d67de7899a0
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-11 12:33:06 +0200

    added entry points with dumb text

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/exercise/new-user", (req, res) => {
+  res.json({ name: "fuckall" });
+});
 
+app.post("/api/exercise/add", (req, res) => {
+  res.json({ name: "golololo" });
+});
 
+app.get("/api/exercise/users", (req, res) => {
+  res.json({ name: "erererer" });
+});
 
+app.get("/api/exercise/log", (req, res) => {
+  res.json({ name: "as;dklsa;ldkweotijo" });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
