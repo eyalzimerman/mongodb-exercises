c0421b6b7eeb62af485d6616b3855fccbf6a7581
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-11 13:35:35 +0200

    added one post request

--- model/user.js
+++ model/user.js
@@ -14,7 +14,7 @@ mongoose
   });
 
 const userSchema = new mongoose.Schema({
-  userName: { type: String, required: true },
+  username: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const User = require("./model/user");
+const Exercise = require("./model/exercise");
 const app = express();
 const cors = require("cors");
 
@@ -10,10 +12,23 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/exercise/new-user", (req, res) => {
+app.post("/api/exercise/new-user", async (req, res) => {
   const { username } = req.body;
 
-  res.json({ name: "fuckall" });
+  try {
+    const isUnique = await User.find({ username: username });
+    if (isUnique.length !== 0) {
+      return res.status(400).json("User already exist");
+    }
+    const newUser = new User({
+      username: username,
+    });
+    newUser.save().then((result) => {
+      res.status(200).json(result);
+    });
+  } catch {
+    return res.status(500).send({ error: "Problems with our server" });
+  }
 });
 
 app.post("/api/exercise/add", (req, res) => {
